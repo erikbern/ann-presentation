ff9afe8d610fbe97c19c6b8130db99efc9668227
Erik Bernhardsson <mail@erikbern.com>
2015-08-30 16:36:42 -0400

    use Visitor pattern

--- tree.py
+++ tree.py
@@ -7,33 +7,18 @@ import matplotlib.pyplot as plt
 from colorsys import hsv_to_rgb
 from voronoi import voronoi_polygons
 
-fig, ax = plt.subplots()
+def split_points(ax, poly, points, voronoi, indices, lw=3.0, lo=0.0, hi=5.0/6.0, visitor=None, max_splits=99999, draw_splits=True):
+    random.seed(','.join([str(i) for i in indices]))
 
-def split_points(poly, points, voronoi, indices, lw=3.0, lo=0.0, hi=5.0/6.0, method='tree'):
-    if len(indices) <= 1:
+    if len(indices) <= 1 or max_splits == 0:
         x = [points[i][0] for i in indices]
         y = [points[i][1] for i in indices]
-        c = hsv_to_rgb((lo+hi)/2, 1, 1)
+        # c = hsv_to_rgb((lo+hi)/2, 1, 1)
+        c = hsv_to_rgb(random.random()*5.0/6.0, 0.7+random.random()*0.3, 0.7+random.random()*0.3)
 
         poly_vor = cascaded_union([sg.Polygon(voronoi[i]) for i in indices])
 
-        if method == 'tree':
-            pass
-        elif method == 'voronoi':
-            poly = poly_vor
-        elif method == 'diff':
-            poly = poly.symmetric_difference(poly_vor)
-            c = 'red'
-
-        if poly.geom_type == 'Polygon':
-            polys = [poly]
-        else:
-            polys = poly.geoms
-
-        for poly in polys:
-            ax.add_patch(PolygonPatch(poly, fc=c, lw=0, zorder=0))
-        plt.scatter(x, y, marker='x', zorder=99, c='black', s=1.0)
-
+        visitor(ax, poly, poly_vor, c, x, y)
         return
 
     p1, p2 = [points[i] for i in random.sample(indices, 2)]
@@ -47,27 +32,72 @@ def split_points(poly, points, voronoi, indices, lw=3.0, lo=0.0, hi=5.0/6.0, met
     halfplane_a = sg.Polygon(np.array([m+v_perp*big,  m+v*big, m-v_perp*big])).intersection(poly)
     halfplane_b = sg.Polygon(np.array([m+v_perp*big,  m-v*big, m-v_perp*big])).intersection(poly)
 
-    ax.add_patch(PolygonPatch(halfplane_a, fc='none', lw=lw, zorder=1))
-    ax.add_patch(PolygonPatch(halfplane_b, fc='none', lw=lw, zorder=1))
+    if draw_splits:
+        ax.add_patch(PolygonPatch(halfplane_a, fc='none', lw=lw, zorder=1))
+        ax.add_patch(PolygonPatch(halfplane_b, fc='none', lw=lw, zorder=1))
 
     indices_a = [i for i in indices if np.dot(points[i], v)-a > 0]
     indices_b = [i for i in indices if np.dot(points[i], v)-a < 0]
 
-    split_points(halfplane_a, points, voronoi, indices_a, lw*0.8, lo, (lo+hi)/2, method)
-    split_points(halfplane_b, points, voronoi, indices_b, lw*0.8, (lo+hi)/2, hi, method)
+    split_points(ax, halfplane_a, points, voronoi, indices_a, lw*0.8, lo, (lo+hi)/2, visitor, max_splits-1, draw_splits)
+    split_points(ax, halfplane_b, points, voronoi, indices_b, lw*0.8, (lo+hi)/2, hi, visitor, max_splits-1, draw_splits)
 
-if __name__ == '__main__':
-    points = np.random.randn(250, 2)
+def draw_poly(ax, poly, c, lw=0):
+    if poly.geom_type == 'Polygon':
+        polys = [poly]
+    else:
+        polys = poly.geoms
+
+    for poly in polys:
+        ax.add_patch(PolygonPatch(poly, fc=c, lw=lw, zorder=0))
+
+def scatter(ax, x, y):
+    plt.scatter(x, y, marker='x', zorder=99, c='black', s=10.0)
+
+def tree_visitor(ax, poly, poly_vor, c, x, y):
+    draw_poly(ax, poly, c)
+    scatter(ax, x, y)
+
+def voro_visitor(ax, poly, poly_vor, c, x, y):
+    draw_poly(ax, poly_vor, c, lw=0.2)
+    scatter(ax, x, y)
+
+def null_visitor(ax, poly, poly_vor, c, x, y):
+    scatter(ax, x, y)
+
+def main():
+    np.random.seed(4)
+    points = np.random.randn(200, 2)
     voronoi = voronoi_polygons(points)
 
     inf = 1e9
     plane = sg.Polygon([(inf,inf), (inf,-inf), (-inf,-inf), (-inf,inf)])
 
-    split_points(plane, points, voronoi, range(len(points)), method='voronoi')
+    plots = [('none', null_visitor, 999, False),
+            ('voronoi', voro_visitor, 999, False),
+            ('tree', tree_visitor, 1, True),
+            ('tree', tree_visitor, 2, True),
+            ('tree', tree_visitor, 3, True),
+            ('tree', tree_visitor, 999, True),
+            ('voronoi', voro_visitor, 1, True),
+            ('voronoi', voro_visitor, 2, True),
+            ('voronoi', voro_visitor, 3, True)]
 
-    plt.axis('equal')
-    plt.axis('off')
-    plt.xlim(-2.5, 2.5)
-    plt.ylim(-2.5, 2.5)
-    # plt.show()
-    plt.savefig('tree.png', dpi=1200, bbox_inches='tight', pad_inches=0, transparent=True)
+    for tag, visitor, max_splits, draw_splits in plots:
+        fn = 'tree-%s-%d-%s.png' % (tag, max_splits, draw_splits)
+        print fn, '...'
+
+        fig, ax = plt.subplots()
+
+        split_points(ax, plane, points, voronoi, range(len(points)), visitor=visitor, max_splits=max_splits, draw_splits=draw_splits)
+
+        plt.xlim(-2.5, 2.5)
+        plt.ylim(-2.5, 2.5)
+        
+        plt.axis('off')
+        ax.get_xaxis().set_visible(False)
+        ax.get_yaxis().set_visible(False)
+        plt.savefig(fn, dpi=300, bbox_inches='tight', pad_inches=0, transparent=True)
+
+if __name__ == '__main__':
+    main()
