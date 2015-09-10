dcd0b6cccb3e2af33c7c814678ebc78c2ce0a1bb
Erik Bernhardsson <mail@erikbern.com>
2015-09-10 11:11:01 -0400

    More fixes to compare synthetic vs real data

--- knn_avg_dist.py
+++ knn_avg_dist.py
@@ -3,28 +3,18 @@ from scipy.spatial import distance
 import matplotlib.pyplot as plt
 import random
 import sys
-import gzip
+from util import get_vectors
 
 n = 10000
 
 def get_gaussian(n, f):
     return [numpy.random.normal(0, 1, f) for x in xrange(n)]
 
-def get_vectors(fn, n):
-    dataset = []
-    for line in gzip.open(fn):
-        items = line.strip().split()
-        dataset.append(numpy.array([float(x) for x in items[1:]]))
-        if len(dataset) == n:
-            break
-    print len(dataset)
-    return dataset
-
-def get_avgs(dataset, n_iters=40):
+def get_avgs(dataset, n_iters=40, d=distance.euclidean):
     sums = numpy.zeros(3)
     for i in xrange(n_iters):
         k = random.choice(xrange(len(dataset)))
-        dists = [distance.euclidean(dataset[j], dataset[k]) for j in xrange(len(dataset)) if j != k]
+        dists = [d(dataset[j], dataset[k]) for j in xrange(len(dataset)) if j != k]
         dists.sort()
         sums += numpy.array([dists[0], dists[9], dists[-1]])
         print sums / (i+1)
@@ -38,16 +28,17 @@ for f in fs_synt:
     avgs = get_avgs(dataset)
     ts_synt.append(avgs)
 
-fs_ts_real = []
+fs_real = []
+ts_real = []
 for fn in sys.argv[1:]:
-    dataset = get_vectors(fn, n)
+    print fn, '...'
+    dataset = [numpy.array(x) for item, x in get_vectors(fn, n)]
     f = len(dataset[0])
-    print f, '...'
-    avgs = get_avgs(dataset)
-    fs_ts_real.append((f, avgs))
-fs_ts_real.sort()
-fs_real = [f for f, t in fs_ts_real]
-ts_real = [t for f, t in fs_ts_real]
+    avgs = get_avgs(dataset, d=distance.cosine)
+    fs_real.append(f)
+    ts_real.append(avgs)
+
+print fs_real, ts_real
 
 def configure_ax():
     fig, ax = plt.subplots()
@@ -55,7 +46,7 @@ def configure_ax():
     ax.set_yscale('log')
     ax.grid(True, 'both')
     ax.set_xlabel('Number of dimensions')
-    ax.set_ylabel('Euclidean distance')
+    ax.set_ylabel('Euclidean/cosine distance')
     return fig, ax
 
 fig, ax = configure_ax()
@@ -72,13 +63,13 @@ ax.annotate('Every point\'s neighborhood is the same!',
 )
 fig.savefig('knn_avg_dist_synt.png', dpi=600, bbox_inches='tight', pad_inches=0, transparent=True)
 
-ratio_synt = [ts_synt[i][2] / ts_synt[i][0] for i in xrange(len(ts_synt))]
-ratio_real = [ts_real[i][2] / ts_real[i][0] for i in xrange(len(ts_real))]
+ratio_synt = [(t[2] - t[0]) / t[0] for t in ts_synt]
+ratio_real = [(t[2] - t[0]) / t[0] for t in ts_real]
 
 fig, ax = configure_ax()
 ax.plot(fs_synt, ratio_synt, 'x-', ms=10, mew=5, c='red')
-ax.plot(fs_real, ratio_real, 'x-', ms=10, mew=5, c='blue')
-ax.legend(['Synthetic data: furthest/closest distance ratio',
-           'Real data: furthest/closest distance ratio'], loc=1)
+ax.plot(fs_real, ratio_real, 'x', ms=10, mew=5, c='blue')
+ax.legend(['Synthetic data: (furthest-closest)/closest avg ratio',
+           'Real data: (furthest-closest)/closest avg ratio'], loc=1)
 ax.set_title('%d points from real word vectors' % n)
 fig.savefig('knn_avg_dist_real_vs_synt.png', dpi=600, bbox_inches='tight', pad_inches=0, transparent=True)
--- util.py
+++ util.py
@@ -1,17 +1,15 @@
+import gzip
 import struct
-
-def _get_vectors_plain(f):
-    for line in f:
-        items = line.strip().split()
-        yield items[0], [float(x) for x in items[1:]]
 
 def _get_vectors(fn):
     if fn.endswith('.gz'):
-        for line in _get_vectors_plain(gzip.open(fn)):
-            yield line
+        f = gzip.open(fn)
+        fn = fn[:-3]
 
-    elif fn.endswith('.bin'): # word2vec format
+    else:
         f = open(fn)
+
+    if fn.endswith('.bin'): # word2vec format
         words, size = (int(x) for x in f.readline().strip().split())
 
         t = 'f' * size
@@ -28,9 +26,10 @@ def _get_vectors(fn):
 
             yield word, vec
 
-    else: # Assume simple text format
-        for line in _get_vectors_plain(open(fn)):
-            yield line
+    elif fn.endswith('.txt'): # Assume simple text format
+        for line in f:
+            items = line.strip().split()
+            yield items[0], [float(x) for x in items[1:]]
 
 def get_vectors(fn, n=float('inf')):
     i = 0
