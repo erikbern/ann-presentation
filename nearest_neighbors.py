6dc46ef8cad37945eb0e5f6a818db9a9170a4f4c
Erik Bernhardsson <mail@erikbern.com>
2015-09-14 21:16:23 -0400

    nearest neighbors now compatible with word2vec

--- nearest_neighbors.py
+++ nearest_neighbors.py
@@ -1,5 +1,7 @@
 '''
-Usage: python nearest_neighbors.py <data file> <word>
+Usage: python nearest_neighbors.py <data file> [<search k>]
+
+(works like ./distance in word2vec)
 
 * Data file can be
   1. A .bin file that is compatible with word2vec binary formats.
@@ -15,19 +17,23 @@ import lmdb
 import os
 import struct
 import sys
+import numpy
 
 from util import get_vectors
 
-try:
-    fn, word1 = sys.argv[1:]
-except:
+if len(sys.argv) < 2:
     print __doc__
     raise
+
+fn = sys.argv[1]
+search_k = 100000
+if len(sys.argv) > 2:
+    search_k = int(sys.argv[2])
 
 fn_annoy = fn + '.annoy'
 fn_lmdb = fn + '.lmdb' # stores word <-> id mapping
 
-word, vec = get_vectors(sys.argv[1]).next()
+word, vec = get_vectors(fn).next()
 size = len(vec)
 env = lmdb.open(fn_lmdb, map_size=int(1e9))
 
@@ -52,10 +58,18 @@ a = annoy.AnnoyIndex(size)
 a.load(fn_annoy)
 
 with env.begin() as txn:
-    word = txn.get('w' + word1)
-    id = int(word[1:])
+    for line in sys.stdin:
+        if line.strip() == 'EXIT':
+            break
 
-    for id2 in a.get_nns_by_item(id, 10, 1000):
-        word2 = txn.get('i%d' % id2)[1:]
-        print word2
+        words = line.strip().split()
+        ids = [int(txn.get('w' + word)[1:]) for word in words]
+        vs = [a.get_item_vector(id) for id in ids]
+        vs = [v / numpy.dot(v, v)**0.5 for v in vs]
+        v = numpy.sum(vs, axis=0)
+
+        for id, dist in zip(*a.get_nns_by_vector(v, 20, search_k, True)):
+            if id not in ids:
+                word = txn.get('i%d' % id)[1:]
+                print '%50s\t%f' % (word, dist)
 
